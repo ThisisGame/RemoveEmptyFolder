845c244720b526b2f2431065aef4945241d3dea0
陈鹏 <980097191@qq.com>
2018-11-01 11:18:33 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RemoveEmptyFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tmpDirs = Directory.GetDirectories("./", "*.*", SearchOption.AllDirectories);
+            for (int i = 0; i < tmpDirs.Length; i++)
+            {
+                string tmpDir = tmpDirs[i];
+                if(Directory.Exists(tmpDir))
+                {
+                    string[] tmpFilesInDir = Directory.GetFiles(tmpDir, "*.*", SearchOption.AllDirectories);
+                    if (tmpFilesInDir.Length == 0)
+                    {
+                        Directory.Delete(tmpDir, true);
+
+                        Console.WriteLine(tmpDir);
+                    }
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
